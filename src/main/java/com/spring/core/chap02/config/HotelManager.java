d7ec20352247b82d92926182c0ce9ca2096c754f
youngsik823 <109953656+youngsik823@users.noreply.github.com>
2023-04-18 15:14:13 +0900

    커밋커밋

--- src/main/java/com/spring/core/chap02/config/HotelManager.java
+++ src/main/java/com/spring/core/chap02/config/HotelManager.java
@@ -1,8 +1,15 @@
 package com.spring.core.chap02.config;
 
+import com.spring.core.chap02.Chef;
+import com.spring.core.chap02.KimuraChef;
+
 // 호텔을 운영하기 위한 객체들을 만들어서
 // 호텔에 주입해주는 클래스
 // Spring Container: Bean Factory
 public class HotelManager {
+
+    public Chef chef() {
+        return new KimuraChef();
+    }
 
 }

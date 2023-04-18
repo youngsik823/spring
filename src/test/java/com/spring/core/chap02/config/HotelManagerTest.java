0e37a372e59c466c70566e7cd96217dbe08baf25
youngsik823 <109953656+youngsik823@users.noreply.github.com>
2023-04-18 15:22:21 +0900

    커밋커밋

---
+++ src/test/java/com/spring/core/chap02/config/HotelManagerTest.java
@@ -0,0 +1,17 @@
+package com.spring.core.chap02.config;
+
+import com.spring.core.chap02.Hotel;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HotelManagerTest {
+
+    @Test
+    void diTest() {
+        HotelManager manager = new HotelManager();
+        Hotel hotel = new Hotel();
+
+    }
+
+}

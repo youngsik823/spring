465d5aa3db7137f8894f86ad26f284fd6dca679a
youngsik823 <109953656+youngsik823@users.noreply.github.com>
2023-04-18 15:11:00 +0900

    커밋커밋

--- src/main/java/com/spring/core/chap02/EsternRestaurant.java
+++ src/main/java/com/spring/core/chap02/EsternRestaurant.java
@@ -8,6 +8,11 @@ public class EsternRestaurant implements Restaurant {
     // 요리 코스
     private Course course;
 
+    public EsternRestaurant(Chef chef, Course course) {
+        this.chef = chef;
+        this.course = course;
+    }
+
     // 요리를 주문하는 기능
     public void order() {
         System.out.println("아시안 요리를 주문합니다.");
--- src/main/java/com/spring/core/chap02/WesternRestaurant.java
+++ src/main/java/com/spring/core/chap02/WesternRestaurant.java
@@ -8,6 +8,11 @@ public class WesternRestaurant implements Restaurant {
     // 요리 코스
     private Course course = new FrenchCourse();
 
+    public WesternRestaurant(Chef chef, Course course) {
+        this.chef = chef;
+        this.course = course;
+    }
+
     // 요리를 주문하는 기능
     public void order() {
         System.out.println("서양 요리를 주문합니다.");
